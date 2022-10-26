a310761d8a1f4a7efcc2d2d6b7858f0235559389
muac404 <muac404@gmail.com>
2022-10-26 20:02:15 +0200

    Aggiunge javascript che non funziona

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 "use strict";
-
+// Creo array immagini
 const images = ["img/01.jpg","img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"]
 
 console.log(images)
+// Definisco il contenitore delle immagini
+const items = document.querySelector(".items")
 
+// Ciclo per 
 for (let i = 0; i < images.length; i++) {
+   
+    items.append(`<div class="item"><img src="${images[i]}" alt="img"></div>`)
     
+    console.log(images[i])
     
 }
+
+
+
