a2c72b310415558ac52ba7f21aefa311990f209f
muac404 <muac404@gmail.com>
2022-10-26 17:14:59 +0200

    Aggiunge array javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const images = ["img/01.jpg","img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"]
+
+console.log(images)
+
+for (let i = 0; i < images.length; i++) {
+    
+    
+}
